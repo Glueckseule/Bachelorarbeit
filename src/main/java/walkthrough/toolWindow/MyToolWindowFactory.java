83e97f7cfde2ca0706d8deac0b8b1fe63ba3de39
Glueckseule <30928455+Glueckseule@users.noreply.github.com>
2020-03-29 16:00:12 +0200

    Fix toolWindow state * content not gone after toolWindow hidden * subscribed message bus to all toolwindows, listen for specific * not sure if manager needed

--- src/main/java/walkthrough/toolWindow/MyToolWindowFactory.java
+++ src/main/java/walkthrough/toolWindow/MyToolWindowFactory.java
@@ -2,27 +2,75 @@ package walkthrough.toolWindow;
 
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.wm.ToolWindow;
-import com.intellij.openapi.wm.ToolWindowAnchor;
 import com.intellij.openapi.wm.ToolWindowFactory;
 import com.intellij.openapi.wm.ToolWindowManager;
+import com.intellij.openapi.wm.ex.ToolWindowManagerListener;
 import com.intellij.ui.content.Content;
 import com.intellij.ui.content.ContentFactory;
 import org.jetbrains.annotations.NotNull;
 
+/**
+ * This is the entry point for the plugin.
+ * It states that the essential component of the plugin is a tool window
+ * and creates it (PluginContainer)
+ * The content set comes from the PluginContainer's method getContent()
+ *
+ *
+ * @author Daniela
+ */
 public class MyToolWindowFactory implements ToolWindowFactory {
 
-    /**
-     * This is the entry point for the plugin.
-     * It states that the essential component of the plugin is a tool window
-     * and creates it (PluginContainer)
-     * The content set comes from the PluginContainer's method getContent()
-     */
+    private ToolWindowManager manager;
+    private Project project;
+    private ToolWindow toolWindow;
+    private ContentContainer contentContainer;
 
+    /**
+     * register toolWindow to the manager
+     * init content of toolWindow
+     * set a listener on toolWindow which listens to when toolWindow is minimized/reopened
+     *
+     * @param project       The project that is opened in the plugin IDE
+     * @param toolWindow    ToolWindow with the id "Walkthrough durch IntelliJ"
+     */
+    @Override
     public void createToolWindowContent(@NotNull Project project, @NotNull ToolWindow toolWindow) {
-        PluginContainer myToolWindow = new PluginContainer(project);
-        ContentFactory contentFactory = ContentFactory.SERVICE.getInstance();
-        Content content = contentFactory.createContent(myToolWindow.getContent(), "", false);
-        toolWindow.getContentManager().addContent(content);
+        this.project = project;
+        this.toolWindow = toolWindow;
+        initResources();
+        register();
     }
 
+    /**
+     * TODO: Register the Tool Window to the manager, so we can handle all toolWindow things there
+     */
+    private void register() {
+        manager = ToolWindowManager.getInstance(project);
+        manager.getToolWindow(toolWindow.getTitle());
+
+        project.getMessageBus().connect().subscribe(ToolWindowManagerListener.TOPIC, new ToolWindowManagerListener() {
+            @Override
+            public void stateChanged() {
+                if (toolWindow.isVisible()) {
+                    System.out.println("STATE CHANGED: Now here");
+                    initResources();
+                    setContent();
+                } else {
+                    System.out.println("STATE CHANGED: gone");
+                }
+            }
+        });
+    }
+
+    /**
+     * Create the container in toolWindow in which the the content will be loaded into.
+     */
+    private void initResources() {
+        contentContainer = new ContentContainer(project);
+    }
+
+    private void setContent(){
+        Content webViewContent = ContentFactory.SERVICE.getInstance().createContent(contentContainer.getContent(), "", false);
+        toolWindow.getContentManager().addContent(webViewContent);
+    }
 }
